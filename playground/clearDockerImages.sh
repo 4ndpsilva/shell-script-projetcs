05e70228ce25ca9b7baba872397bd165b9440fde
André <4ndpsilva@gmail.com>
2020-12-20 06:38:39 -0300

    Modificações das condicionais

--- playground/clearDockerImages.sh
+++ playground/clearDockerImages.sh
@@ -15,32 +15,48 @@
 #
 # Exit immediately if a command exits with a non-zero status
 set -e
-source ./functions.sh
+#source ./functions.sh
 
 
-checkParams "clearDockerImages"
+#checkParams "clearDockerImages"
 
 
-setBusyStatus "CLEAR"
+#etBusyStatus "CLEAR"
 
 #Ambiente - Local ou Remoto
-ENV=$1
+ENVIRONMENT=$1
 
-#Tag de imagem docker
+#Tag da imagem docker
 TAG=$2
 
-if [ ! -z $ENV ]; then
-    if [ -z $TAG ]; then
-        echo "É necessário informar uma tag para exclusão" >&2
-        exit 1
-    elif [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
-        echo "APAGAR IMAGEM COM A TAG [ $TAG ]"
-        docker rmi --force $(docker images | grep $TAG | awk '{ print $3 }')
-    else
-        echo "Não é permitido apagar imagens com as tags 'latest' ou 'current'" >&2
-        exit 1
-    fi
-else
-    echo "É necessário informar um ambiente de execução" >&2
+if [ ! -z $ENVIRONMENT ]; then
+  EXIST_ENV=$(docker images | grep $ENVIRONMENT)
+  
+  if [ -z $EXIST_ENV ]; then
+    echo "O ambiente informado não existe" >&2
     exit 1
+  else
+    if [ ! -z $TAG ]; then
+      EXIST_TAG=$(docker images | grep $TAG)
+    
+      if [ -z $EXIST_TAG ]; then
+        echo "A tag informada não existe" >&2
+        exit 1
+      else
+        if [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
+          echo "APAGAR IMAGEM COM A TAG [ $TAG ]"
+          docker rmi --force $(docker images | grep $ENVIRONMENT | grep $TAG | awk '{ print $3 }')
+        else
+          echo "Não é permitido apagar imagens com as tags 'latest' ou 'current'" >&2
+          exit 1
+        fi
+      fi      
+    else   
+      echo "É necessário informar uma tag" >&2
+      exit 1
+    fi    
+  fi
+else
+  echo "É necessário informar um ambiente" >&2
+  exit 1
 fi
--- playground/my-functions.sh
+++ playground/my-functions.sh
@@ -5,7 +5,7 @@ function verifyAvailableSpace(){
   AVAILABLE_SPACE=$(df | awk '/ebs/ { print $4 }')
 
   #Converte de Gigabyte para Kilobyte
-  REQUIRED_SPACE=$(expr $(($1 * 1024 * 1024)))
+  REQUIRED_SPACE=$(($1 * 1024 * 1024))
 
   if [ $REQUIRED_SPACE -gt $AVAILABLE_SPACE ]; then
     echo "Espaço em disco insuficiente" >&2
@@ -14,24 +14,40 @@ function verifyAvailableSpace(){
 }
 
 function clearImage(){
-  #Tag de imagem docker
-  TAG=$1
-  #Ambiente - Local ou Remoto
-  ENV=$2
+  ENVIRONMENT=$1
 
-  if [ ! -z $ENV ]; then
-      if [ -z $TAG ]; then
-          echo "É necessário informar uma tag para exclusão" >&2
-          exit 1
-      elif [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
-          echo "APAGAR IMAGEM COM A TAG [ $TAG ]"
-          docker rmi --force $(docker images | grep $TAG | awk '{ print $3 }')
-      else
-          echo "Não é permitido apagar imagens com as tags 'latest' ou 'current'" >&2
-          exit 1
-      fi
-  else
-      echo "É necessário informar um ambiente de execução" >&2
+  #Tag da imagem docker
+  TAG=$2
+
+  if [ ! -z $ENVIRONMENT ]; then
+    EXIST_ENV=$(docker images | grep $ENVIRONMENT)
+    
+    if [ -z $EXIST_ENV ]; then
+      echo "O ambiente informado não existe" >&2
       exit 1
+    else
+      if [ ! -z $TAG ]; then
+        EXIST_TAG=$(docker images | grep $TAG)
+      
+        if [ -z $EXIST_TAG ]; then
+          echo "A tag informada não existe" >&2
+          exit 1
+        else
+          if [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
+            echo "APAGAR IMAGEM COM A TAG [ $TAG ]"
+            docker rmi --force $(docker images | grep $ENVIRONMENT | grep $TAG | awk '{ print $3 }')
+          else
+            echo "Não é permitido apagar imagens com as tags 'latest' ou 'current'" >&2
+            exit 1
+          fi
+        fi      
+      else   
+        echo "É necessário informar uma tag" >&2
+        exit 1
+      fi    
+    fi
+  else
+    echo "É necessário informar um ambiente" >&2
+    exit 1
   fi
 }
