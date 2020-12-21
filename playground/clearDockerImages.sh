1bb1e3f9007a48fd4b38ae921f3531748b2ed1d6
André <4ndpsilva@gmail.com>
2020-12-21 18:14:35 -0300

    Endpoint de limpeza de imagens

--- playground/clearDockerImages.sh
+++ playground/clearDockerImages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #=======================================================================
 #
 #        FILE: .../docker-db/clearDockerImages.sh
@@ -14,47 +15,51 @@
 #set -x
 #
 # Exit immediately if a command exits with a non-zero status
+
 set -e
-#source ./functions.sh
+
+source ./functions.sh
 
 
-#checkParams "clearDockerImages"
+checkParams "clearDockerImages"
+
+setBusyStatus "CLEAR"
 
 
-#etBusyStatus "CLEAR"
-
-#Ambiente - Local ou Remoto
+#Ambiente
 ENVIRONMENT=$1
+
 
 #Tag da imagem docker
 TAG=$2
 
 if [ ! -z $ENVIRONMENT ]; then
   EXIST_ENV=$(docker images | grep -w $ENVIRONMENT | awk '{ print $1 }' | sed -n 1p)
-  
+
   if [ -z $EXIST_ENV ]; then
     echo "O ambiente informado não existe" >&2
     exit 1
   else
     if [ ! -z $TAG ]; then
       EXIST_TAG=$(docker images | grep -w $ENVIRONMENT | grep -w $TAG | awk '{ print $2 }' | sed -n 1p)
-    
+
       if [ -z $EXIST_TAG ]; then
         echo "A tag informada não existe" >&2
         exit 1
       else
-        if [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
-          echo "APAGAR IMAGEM COM A TAG [ $TAG ]"
+	if [[ $TAG != *"latest"* ]] && [[ $TAG != *"current"* ]]; then
           docker rmi -f $(docker images | grep -w $ENVIRONMENT | grep -w $TAG | awk '{ print $3 }')
+          echo "Imagem [ $TAG ] excluída com sucesso" >&1 
+          exit 0
         else
           echo "Não é permitido apagar imagens com as tags 'latest' ou 'current'" >&2
           exit 1
         fi
-      fi      
-    else   
+      fi
+    else
       echo "É necessário informar uma tag" >&2
       exit 1
-    fi    
+    fi
   fi
 else
   echo "É necessário informar um ambiente" >&2
