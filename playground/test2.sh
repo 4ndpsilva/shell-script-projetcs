77804e7d300ae56ff1f7607a5af82fe898b111e2
André <4ndpsilva@gmail.com>
2021-02-01 16:42:04 -0300

    Implementaçao da lista de imagens sem hashes repetidos

--- playground/test2.sh
+++ playground/test2.sh
@@ -2,32 +2,32 @@
 
 
 function build() {
-  mapAllHashes=$1
-  # selecionar as hashes sem repetição
-  for imageTag in ${keys[@]}; do
-    hashes=(${mapAllHashes[$imageTag]})
-    hash=${hashes[0]}
-    
-    for key in ${keys[@]}; do
-      if [ "$imageTag" != "$key" ]; then
-        hashesToCompare=(${mapAllHashes[$key]})
-        qtd=0
+i=0
+for key in ${!mapUnique[@]}; do
+  hashes=(${mapUnique[$key]})
+  topHash=${hashes[0]}
+  echo $topHash
+  
+  for k in ${!mapUnique[@]}; do
+    if [ "$key" != "$k" ]; then
+      hashesToCompare=(${mapUnique[$key]})
+      qtd=0
+  
+      for h in ${hashesToCompare[@]}; do
+        if [ "$topHash" != "$h" ]; then
+          qtd=$((qtd + 1))      
+        fi
+      done
 
-        for h in ${hashesToCompare[@]}; do
-          if [ "$hash" = "$h" ]; then
-            qtd=$((qtd + 1))      
-          fi
-        done
-
-        if [ $qtd -eq 0 ]; then
-          mapUniqueHashes[$imageTag]=$hash
-          break
-        fi    
-      fi
-    done
-  done       
-
-  build ${mapUniqueHashes[@]}
+      if [ $qtd -eq 0 ]; then
+        sheets[$i]=$key
+        i=$((i + 1))
+        echo ${sheets[$i]}
+        break
+      fi    
+    fi
+  done
+done     
 }
 
 
@@ -41,3 +41,23 @@ for imageTag in ${keys[@]}; do
 done
 
 echo ""
+
+
+
+
+
+: '
+# Criação do map no formato image:tag -> (lista de hashes)
+for imageTag in ${keys[@]}; do
+  hashes=()
+  i=0  
+  
+  for hash in $(docker history $imageTag | grep -vi IMAGE | grep -v "<missing>" | awk '{ print $1 }'); do
+    hashes[i]=$hash
+    i=$((i + 1))
+  done
+  
+  mapHistoryImages[$imageTag]=${hashes[@]}
+  mapSizes[$imageTag]=$(docker images $imageTag | grep -vi IMAGE | awk '{print $NF}')
+done
+'
--- playground/tree-images.sh
+++ playground/tree-images.sh
@@ -37,68 +37,105 @@ done
 
 
 
-
+# Monta um map com os primeiros hashes dos históricos de cada imagem
 declare -A mapImages
 
-for imageTag in ${keys[@]}; do
-  hashes=(${mapHistoryImages[$imageTag]})
-  mapImages[$imageTag]=${hashes[0]}
+for key in ${!mapHistoryImages[@]}; do
+  hashes=(${mapHistoryImages[$key]})
+  mapImages[$key]=${hashes[0]}
 done
 
 
-# montar lista de tags com hashes repetidos
-declare -A mapTags
+# Monta lista de imagens com hashes repetidos (tags)
+function getMapTags(){
+  declare -A mapTags
 
-for key in ${!mapImages[@]}; do
-  hash=${mapImages[$key]}
-  
-  i=0
-  tags=()
+  for key in ${!mapImages[@]}; do
+    hash=${mapImages[$key]}
+    
+    i=0
+    tags=()
 
-  for k in ${!mapImages[@]}; do
-    if [ "$k" != "$key" ]; then
-      hashToCompare=${mapImages[$k]}
+    for k in ${!mapImages[@]}; do
+      if [ "$k" != "$key" ]; then
+        hashToCompare=${mapImages[$k]}
 
-      if [ "$hash" = "$hashToCompare" ]; then
-        tags[$i]=$k 
+        if [ "$hash" = "$hashToCompare" ]; then
+          tags[$i]=$k 
+          i=$((i + 1))
+        fi    
+      else
+        tags[$i]=$k
         i=$((i + 1))
-      fi    
-    else
-      tags[$i]=$k
+      fi
+    done
+
+    #MODIFICAR ESSA PARTE
+    if [ ${#tags[@]} -gt 0 ]; then
+      mapTags[$key]=${tags[@]}
+    fi
+  done  
+}
+
+
+
+# Imagens que não tem hash repetidos no seu histórico
+function getMapUnique(){
+  declare -A mapUnique
+
+  for key in ${!mapImages[@]}; do
+    has=0
+
+    for tag in ${tags[@]}; do
+      if [ "$key" = "$tag" ]; then
+        has=1
+        break
+      fi
+    done
+
+    if [ $has -eq 0 ]; then
+      mapUnique[$key]=${mapImages[$key]}
+    fi
+  done
+}
+
+
+function getAllHashes(){
+  allHashes=()
+  i=0
+
+  for key in ${!mapHistoryImages[@]}; do
+    hashes=(${mapHistoryImages[$key]})
+    
+    for h in ${hashes[@]}; do 
+      allHashes[$i]=$h
       i=$((i + 1))
-    fi
+    done
   done
+}
 
-# modificar essa parte
-  if [ ${#tags[@]} -gt 0 ]; then
-    mapTags[$key]=${tags[@]}
+
+function getSheets(){
+  getAllHashes
+
+  declare -A sheets
+
+  if [ ${#mapUnique[@]} -gt 0 ] ; then
+    for key in ${!mapUnique[@]}; do
+      hashes=(${mapUnique[$key]})
+      topHash=${hashes[0]}
+      
+      local qtd=0
+
+      for hash in ${allHashes[@]}; do
+        if [ "$hash" = "$topHash" ]; then
+          qtd=$((qtd + 1))
+        fi
+      done
+
+      if [ $qtd = 1 ]; then
+        sheets[$key]=$topHash
+      fi
+    done
   fi
-done  
-
-
-# Imagens que não tem hash repetidos
-declare -A mapUnique
-
-for key in ${!mapImages[@]}; do
-  has=0
-  for tag in ${tags[@]}; do
-    if [ "$key" = "$tag" ]; then
-      has=1
-      break
-    fi
-  done
-
-  if [ $has -eq 0 ]; then
-    mapUnique[$key]=${mapImages[$key]}
-  fi
-done
-
-for k in ${!mapHistoryImages[@]}; do
-  echo $k
-  hashes=(${mapHistoryImages[$k]}) 
-  
-  for i in ${hashes[@]}; do
-    echo "---- $i"
-  done
-  echo "" 
-done
+}
