ab0b5402090e0c822bd4598b3be0aeed308d43c3
André <4ndpsilva@gmail.com>
2021-02-22 17:48:28 -0300

    Novos ajustes gerais nas funçoes

--- playground/image-tree.sh
+++ playground/image-tree.sh
@@ -4,7 +4,7 @@
 declare -A mapHistoryImages
 declare -A mapSizes
 
-# Recupera os aliases das imagens no formato imagem:tag - imagem:tag servirá de chave para acesso aos valores dos maps
+###################################### Recupera os aliases das imagens no formato imagem:tag - imagem:tag ######################################
 for alias in $(docker images | grep -v \\scurrent\\s | grep -v \\slatest\\s | awk '{print $1 ":" $2}'); do
   if [[ "$alias" != "REPOSITORY:TAG" ]] && [[ "$alias" != *"<none>"* ]]; then
     hashes=()
@@ -21,44 +21,46 @@ for alias in $(docker images | grep -v \\scurrent\\s | grep -v \\slatest\\s | aw
 done
 
 
-# Monta um map com o primeiro ID do histórico de cada imagem
-declare -A mapImages
+###################################### Monta um map com o primeiro ID do histórico de cada imagem ######################################
+declare -A mapFirstHash
 
-for key in ${!mapHistoryImages[@]}; do
-  hashes=(${mapHistoryImages[$key]})
-  mapImages[$key]=${hashes[0]}
+for alias in ${!mapHistoryImages[@]}; do
+  hashes=(${mapHistoryImages[$alias]})
+  mapFirstHash[$alias]=${hashes[0]}
 done
 
 
-# Monta um array com todos IDs das imagens
+###################################### Monta um array com todos IDs de todas imagens ######################################
 function getAllHashes(){
   allHashes=()
   local i=0
 
-  for key in ${!mapHistoryImages[@]}; do
-    hashes=(${mapHistoryImages[$key]})
+  for alias in ${!mapHistoryImages[@]}; do
+    hashes=(${mapHistoryImages[$alias]})
     
     for h in ${hashes[@]}; do 
       allHashes[$i]=$h
       i=$((i + 1))
     done
+    echo ""
   done
 }
 
 
-# Monta map com as tags de determinada imagem
+###################################### Monta um map com as tags de determinada imagem ######################################
 declare -A mapTags
 
 function getMapTags(){
-  for key in ${!mapImages[@]}; do
-    hash=${mapImages[$key]}
+  for alias in ${!mapFirstHash[@]}; do
+    hash=${mapFirstHash[$alias]}
     
     local i=0
     tags=()
+    echo $hash
 
-    for k in ${!mapImages[@]}; do
-      if [ "$k" != "$key" ]; then
-        hashToCompare=${mapImages[$k]}
+    for k in ${!mapFirstHash[@]}; do
+      if [ "$k" != "$alias" ]; then
+        hashToCompare=${mapFirstHash[$k]}
 
         if [ "$hash" = "$hashToCompare" ]; then
           tags[$i]=$k 
@@ -71,18 +73,19 @@ function getMapTags(){
     done
 
     if [ ${#tags[@]} -gt 1 ]; then
-      mapTags[$key]=${tags[@]}
+      mapTags[$alias]=${tags[@]}
     fi
   done  
 }
 
+getMapTags
 
-# Imagens que não tem IDs repetidos no seu histórico
+###################################### Imagens que não tem IDs repetidos no seu histórico ######################################
 declare -A mapUnique
 
 function getMapUnique(){
 
-  for key in ${!mapImages[@]}; do
+  for key in ${!mapFirstHash[@]}; do
     local has=0
 
     for tag in ${tags[@]}; do
@@ -93,14 +96,13 @@ function getMapUnique(){
     done
 
     if [ $has -eq 0 ]; then
-      mapUnique[$key]=${mapImages[$key]}
+      mapUnique[$key]=${mapFirstHash[$key]}
     fi
   done
 }
 
 
-
-# Monta map com imagens que podem ser apagadas
+###################################### Monta map com imagens que podem ser apagadas ######################################
 declare -A sheets
 
 function getReclaimableImages(){
