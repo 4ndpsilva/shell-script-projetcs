af9d5ecc207df77254eff7696cbf30e2ce751ed0
Andre <4ndpsilva@gmail.com>
2021-03-01 02:31:33 -0300

    Melhorias no script

--- playground/rascunho.sh
+++ playground/rascunho.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
 
-declare -A mapHistoryImages
+declare -A map
+declare -A mapHistory
 declare -A mapSizes
 
 ###################################### Recupera os aliases das imagens no formato imagem:tag - imagem:tag ######################################
-for alias in $(docker images | grep -v \\scurrent\\s | grep -v \\slatest\\s | awk '{print $1 ":" $2}'); do
-  if [[ "$alias" != "REPOSITORY:TAG" ]] && [[ "$alias" != *"<none>"* ]]; then
-    hashes=()
-    i=0  
-    echo $alias
-    for hash in $(docker history $alias | grep -vi IMAGE | grep -v "<missing>" | awk '{ print $1 }'); do
-        hashes[i]=$hash
-        i=$((i + 1))
-        echo "  $hash"
+lastHash=""
+allHashes=()
+x=0
+
+for hash in $(docker images | grep -v \\scurrent\\s | grep -v \\slatest\\s | awk '{print $3}'); do
+  if [ "$hash" != "IMAGE" ]; then
+    if [ "$hash" != "$lastHash" ]; then
+      lastHash=$hash
+      size=$(docker inspect $hash | jq -r '.[].RepoTags | length')
+      
+      if [ $size -gt 0 ]; then
+        i=0  
+        aliases=()
+
+        for((c=0; $c < $size; c++)) do
+          aliases[$i]=$(docker inspect $hash | jq -r ".[].RepoTags[$c]")
+          i=$(($i + 1))
+        done
+
+        map[$hash]=${aliases[@]}
+        mapSizes[$hash]=$(docker images $hash | grep -vi IMAGE | awk '{print $NF}')  
+      fi  
+    fi
+
+    for h in $(docker history $hash | grep -vi IMAGE | grep -v "<missing>" | awk '{ print $1 }'); do
+      allHashes[$x]=$h
+      x=$(($x + 1))
     done
-    echo ""
-    mapHistoryImages[$alias]=${hashes[@]}
-    mapSizes[$alias]=$(docker images $alias | grep -vi IMAGE | awk '{print $NF}')
   fi
+done
+
+for h in ${allHashes[@]}; do
+  echo $h
 done
