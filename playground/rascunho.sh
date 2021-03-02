fd47042e0b391b558ee7bc988b09e67b7d42159b
Andre <4ndpsilva@gmail.com>
2021-03-02 11:24:46 -0300

    ajustes

--- playground/rascunho.sh
+++ playground/rascunho.sh
@@ -10,33 +10,31 @@ lastHash=""
 allHashes=()
 x=0
 
-for hash in $(docker images | grep -v \\scurrent\\s | grep -v \\slatest\\s | awk '{print $3}'); do
-  if [ "$hash" != "IMAGE" ]; then
-    if [ "$hash" != "$lastHash" ]; then
-      lastHash=$hash
-      size=$(docker inspect $hash | jq -r '.[].RepoTags | length')
-      
-      if [ $size -gt 0 ]; then
-        i=0  
-        aliases=()
-
-        for((c=0; $c < $size; c++)) do
-          aliases[$i]=$(docker inspect $hash | jq -r ".[].RepoTags[$c]")
-          i=$(($i + 1))
-        done
-
-        mapAliases[$hash]=${aliases[@]}
-        mapSizes[$hash]=$(docker images $hash | grep -vi IMAGE | awk '{print $NF}')  
-      fi  
-    fi
+for hash in $(docker images | grep -v "IMAGE" | grep -v \\scurrent\\s | grep -v \\slatest\\s | grep -v "<none>" | awk '{print $3}'); do
+  if [ "$hash" != "$lastHash" ]; then
+    lastHash=$hash
+    size=$(docker inspect $hash | jq -r '.[].RepoTags | length')
     
-    for h in $(docker history $hash | grep -vi IMAGE | grep -v "<missing>" | awk '{ print $1 }'); do
-      allHashes[$x]=$h
-      x=$(($x + 1))
-    done
+    if [ $size -gt 0 ]; then
+      i=0  
+      aliases=()
 
-    mapHistory[$hash]=${allHashes[@]}
+      for((c=0; $c < $size; c++)) do
+        aliases[$i]=$(docker inspect $hash | jq -r ".[].RepoTags[$c]")
+        i=$(($i + 1))
+      done
+
+      mapAliases[$hash]=${aliases[@]}
+      mapSizes[$hash]=$(docker images $hash | grep -vi IMAGE | awk '{print $NF}')  
+    fi  
   fi
+  
+  for h in $(docker history $hash | grep -vi IMAGE | grep -v "<missing>" | awk '{ print $1 }'); do
+    allHashes[$x]=$h
+    x=$(($x + 1))
+  done
+
+  mapHistory[$hash]=${allHashes[@]}
 done
 
 
@@ -54,13 +52,15 @@ for hash in ${!mapAliases[@]}; do
 
     if [ $q -gt 1 ]; then
       mapRepeatedHash[$hash]=${mapAliases[$hash]}
+      unset mapAliases[$hash]
     else
       mapUniqueHash[$hash]=${mapAliases[$hash]}
     fi
   done;
 
-for hash in ${!mapHistory[@]}; do
-  hashes=(${mapHistory[$hash]})
+
+for hash in ${!mapAliases[@]}; do
+  #hashes=(${mapUniqueHash[$hash]})
 
   echo ${mapAliases[$hash]}
 done
